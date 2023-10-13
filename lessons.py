95f10b12fa415d1d7c65ab0fc4a77103e51391c4
belvasevg <belvasevg@yandex.ru>
2023-10-13 16:52:50 +0300

    Added protective functions

--- lessons.py
+++ lessons.py
@@ -1,40 +1,41 @@
+import links as l
 lessonsList = ["Урок 1","Урок 2","Урок 3","Урок 4","Урок 5","Урок 6","Урок 7","Урок 8","Урок 9","Урок 10","Урок 11","Урок 12","Урок 13"]
 
-les1Link = "https://gbcdn.mrgcdn.ru/uploads/record/53866/attachment/d982ab675c99aeaa4bb18df227785d63.mp4"
+les1Link = l.link1
 les1Name = "Введение в GIT"
 
-les2Link = "https://gbcdn.mrgcdn.ru/uploads/record/53867/attachment/365728aa81aa8e4f5fddc7519e27f7e5.mp4"
+les2Link = l.link2
 les2Name = "Установка Git на Linux"
 
-les3Link = "https://gbcdn.mrgcdn.ru/uploads/record/53872/attachment/9322568415e87a6e4f19840c7b0c7eb8.mp4"
+les3Link = l.link3
 les3Name = "Git. Команды"
 
-les4Link = "https://gbcdn.mrgcdn.ru/uploads/record/53873/attachment/0f5391d7a94d2e70723659a6d8e8a646.mp4"
+les4Link = l.link4
 les4Name = "Git. Репозитории"
 
-les5Link = "https://gbcdn.mrgcdn.ru/uploads/record/53879/attachment/a8a958e53b121a0fde475e49440831ba.mp4"
+les5Link = l.link5
 les5Name = "Git. Коммиты"
 
-les6Link = "https://gbcdn.mrgcdn.ru/uploads/record/53880/attachment/a02714fbfb68636543b39211cbb99b0f.mp4"
+les6Link = l.link6
 les6Name = "Git. История"
 
-les7Link = "https://gbcdn.mrgcdn.ru/uploads/record/53877/attachment/0b9da951e89f5602b8bebea04a212d7c.mp4"
+les7Link = l.link7
 les7Name = "Git. Ветки"
 
-les8Link = "https://gbcdn.mrgcdn.ru/uploads/record/53876/attachment/323e863b7bb7a72745919f10c0c13362.mp4"
+les8Link = l.link8
 les8Name = "Git. Публикация"
 
-les9Link = "https://gbcdn.mrgcdn.ru/uploads/record/53878/attachment/1e5e598b3858ed2133b7c070241fc6cb.mp4"
+les9Link = l.link9
 les9Name = "Git. Слияния"
 
-les10Link = "https://gbcdn.mrgcdn.ru/uploads/record/53882/attachment/d3d35d4a1e59a31666293b62815fa80f.mp4"
+les10Link = l.link10
 les10Name = "Git. Версионирование"
 
-les11Link = "https://gbcdn.mrgcdn.ru/uploads/record/53875/attachment/6456d74417e057f1b3edb7655ce39d12.mp4"
+les11Link = l.link11
 les11Name = "Git. Pull-requests"
 
-les12Link = "https://gbcdn.mrgcdn.ru/uploads/record/53881/attachment/d210bef9e8826284265ede162c4f6dfb.mp4"
+les12Link = l.link12
 les12Name = "Git. Сложные операции"
 
-les13Link = "https://gbcdn.mrgcdn.ru/uploads/record/53874/attachment/25ecd431e83b13c050a500923bf10a0f.mp4"
+les13Link = l.link13
 les13Name = "Git. Работа с Fork"
